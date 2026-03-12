b5f4ebf8871dba148cf901bf3632d2ab46154fd1
Jake Sykes <jakesykes@proton.me>
2026-03-12 15:35:24 +0000

    added test mapping
    we can now atualy run the website localy!!

--- src/main/java/com/group_project/craft/CraftApplication.java
+++ src/main/java/com/group_project/craft/CraftApplication.java
@@ -2,12 +2,18 @@ package com.group_project.craft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CraftApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CraftApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+	SpringApplication.run(CraftApplication.class, args);
+    }
+    
+    @GetMapping("/test")
+    public String test() {
+	return String.format("ITS ALIVE");
+    }
 }
