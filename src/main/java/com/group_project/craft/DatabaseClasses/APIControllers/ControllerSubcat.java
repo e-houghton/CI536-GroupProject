e2f84e54a2355195f422b91bd5e36a3d36a0ea71
ehoughton <e.houghton4@uni.brighton.ac.uk>
2026-04-23 16:05:52 +0100

    fixed an issue with adding new subcateogories

--- src/main/java/com/group_project/craft/DatabaseClasses/APIControllers/ControllerSubcat.java
+++ src/main/java/com/group_project/craft/DatabaseClasses/APIControllers/ControllerSubcat.java
@@ -1,21 +1,29 @@
 package com.group_project.craft.DatabaseClasses.APIControllers;
 
+import com.group_project.craft.DatabaseClasses.Service.ServiceCategory;
 import com.group_project.craft.DatabaseClasses.Service.ServiceReviewSeller;
 import com.group_project.craft.DatabaseClasses.Service.ServiceSubcat;
 import com.group_project.craft.DatabaseClasses.Tables.ReviewSeller;
 import com.group_project.craft.DatabaseClasses.Tables.Subcategory;
+import jakarta.persistence.AssociationOverride;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/subcat")
 public class ControllerSubcat extends ControllerParent<ServiceSubcat, Subcategory> {
     @Autowired
     ServiceSubcat table;
+    ServiceCategory catTable;
     protected ServiceSubcat getTable(){
         return table;
     }
 
+    /*@PostMapping("/add")
+    public Subcategory create(@RequestBody String description, ){
+        return getTable().addSubcat();
+    }*/
 
 }
